e3cd2569afa0410070e0ab57603ea5c6bc81f18e
mes32 <stockman.mike@gmail.com>
2019-01-12 12:49:38 -0600

    Add some mock client-side functionality to 'client.js'.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,35 @@
-console.log('JavaScript running.');
+class Task {
+    constructor(id, description, completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+}
 
 $(onReady);
 function onReady() {
-    console.log('Document ready - JQ running.');
+    addAllListeners();
+    getTasksFromServer();
+}
+
+function addAllListeners() {
+    $('#add-new-task').on('click', addNewTask);
+    $('#task-list').on('click', '.mark-complete-button', markTaskComplete);
+    $('#task-list').on('click', '.delete-button', deleteTask);
+}
+
+function addNewTask() {
+    console.log('pressed addNewTask() - doing nothing');
+}
+
+function markTaskComplete() {
+    console.log('pressed markTaskComplete() - doing nothing');
+}
+
+function deleteTask() {
+    console.log('pressed deleteTask() - doing nothing');
+}
+
+function getTasksFromServer() {
+    console.log('getTasksFromServer() - doing nothing');
 }
