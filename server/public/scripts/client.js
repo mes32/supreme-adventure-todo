0600cdea9ab368c24c2d8cc7eb5770a81ab7cc3b
mes32 <stockman.mike@gmail.com>
2019-01-13 11:56:05 -0600

    Use library 'SweetAlert' to improve appearance of all error dialogs. Reorganize code for error dialogs into functions.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,78 +15,50 @@ function addAllListeners() {
 
 // Request that the server add a new TO-DO task
 function addNewTask() {
-    const description = $('#new-task-description').val();
-    if (description) {
+    const taskDescription = $('#new-task-description').val();
+    if (taskDescription) {
         $.ajax({
             method: 'POST',
             url: '/tasks',
-            data: { description: description }
+            data: { description: taskDescription }
         }).then(function(responseStatus) {
             getTasksFromServer();
             $('#new-task-description').val('');
         }).catch(function() {
-            const errorMessage = 'Server error 500: Could not add a new TO-DO task.';
-            console.log(errorMessage);
-            alert(errorMessage);
+            const message = `Could not create task '${taskDescription}'.`;
+            serverError(message);
         });
     }
 }
 
 // Request that the server change the status of a given TO-DO task to completed
 function markTaskComplete() {
-    const id = $(this).parent().parent().data('task-id');
+    const task = taskFromDOM(this);
     $.ajax({
         method: 'PUT',
-        url: `/tasks/${id}`
+        url: `/tasks/${task.id}`
     }).then(function (taskList) {
         getTasksFromServer(taskList);
     }).catch(function () {
-        const errorMessage = 'Server error 500: Could not mark TO-DO task as completed.';
-        console.log(errorMessage);
-        alert(errorMessage);
+        const message = `Could not mark task '${task.description}' as complete.`;
+        serverError(message);
     });
 }
 
 // Request that the server delete a given TO-DO task
 function deleteTask() {
-    const $taskRow = $(this).parent().parent();
-    const id = $taskRow.data('task-id');
-    const description = $taskRow.find('.task-description').text();
-    
-    swal({
-        icon: 'warning',
-        title: 'Delete this task?',
-        text: `Description: ${description}`,
-        buttons: {
-            cancel: true,
-            deleteButton: {
-                text: "Delete!",
-                value: true,
-                closeModal: false,
-            }
-        },
-    })
+    const task = taskFromDOM(this);
+    deleteTaskDialog(task)
     .then((value) => {
         if (value) {
-            console.log('delete confirmed');
             $.ajax({
                 method: 'DELETE',
-                url: `/tasks/${id}`,
+                url: `/tasks/${task.id}`,
             }).then(function (taskList) {
                 getTasksFromServer(taskList);
-                swal({
-                    icon: "success",
-                    title: 'Deleted',
-                    text: `Description: ${description}`,
-                });
+                deleteTaskSuccess(task);
             }).catch(function () {
-                const errorMessage = `[Server Error 500] Could not delete: ${description}`;
-                console.log(errorMessage);
-                swal({
-                    icon: "error",
-                    title: 'Uh-Oh 😱',
-                    text: errorMessage,
-                });
+                deleteTaskError(task);
             });
         }
     });
@@ -100,9 +72,8 @@ function getTasksFromServer() {
     }).then(function(taskList) {
         displayTasks(taskList);
     }).catch(function() {
-        const errorMessage = 'Server error 500: Could not get TO-DO tasks.';
-        console.log(errorMessage);
-        alert(errorMessage);
+        const message = 'Could not retrieve list of TO-DO tasks.';
+        serverError(message);
     });
 }
 
@@ -124,4 +95,71 @@ function displayTasks(taskList) {
         }
         $('#task-list').append($taskRow);
     }
+}
+
+// Retieves data about a TODO task by parsing the DOM starting with a 'this' 
+// from a button element. Returns the task's 'id' and 'description' in an 
+// object.
+function taskFromDOM(thisButton) {
+    const $taskRow = $(thisButton).parent().parent();
+    const id = $taskRow.data('task-id');
+    const description = $taskRow.find('.task-description').text();
+    const outputObject = {
+        id: id,
+        description: description,
+    };
+    return outputObject;
+}
+
+// Displays a warning dialog for when the user tries to delete a task
+// Note: Technically this returns a callback to a function that will produce 
+// this dialog. Possibly this is an example of a JavaScript promise?
+function deleteTaskDialog(task) {
+    const dialogFunction = swal({
+        icon: 'warning',
+        title: 'Delete this task?',
+        text: `Description: ${task.description}`,
+        buttons: {
+            cancel: true,
+            deleteButton: {
+                text: "Delete!",
+                value: true,
+                closeModal: false,
+            }
+        },
+    });
+    return dialogFunction;
+}
+
+// Displays a success dialog when a TODO task is successfully deleted
+function deleteTaskSuccess(task) {
+    swal({
+        icon: "success",
+        title: 'Deleted',
+        text: `Description: ${task.description}`,
+    });
+}
+
+// Displays an error dialog when a TODO task could not be deleted. Also logs an
+// error message on the browser console.
+function deleteTaskError(task) {
+    const errorMessage = `[Server Error 500] Could not delete: ${task.description}`;
+    console.log(errorMessage);
+    swal({
+        icon: "error",
+        title: 'Uh-Oh 😱',
+        text: errorMessage,
+    });
+}
+
+// Displays an error dialog when there was a problem on the server. Also logs an
+// error message on the browser console.
+function serverError(message) {
+    const fullMessage = `[Server Error 500] ${message}`;
+    console.log(fullMessage);
+    swal({
+        icon: "error",
+        title: 'Server Error',
+        text: fullMessage,
+    });
 }
